99df81b20754b3064e1f90b7b38d8fd6f90dd7e7
yuliakotel <yuliakotel@gmail.com>
2024-09-18 16:00:10 -0400

    i returned everything to how it was

--- src/n_n_o/n_n_sumcheck.rs
+++ src/n_n_o/n_n_sumcheck.rs
@@ -1,19 +1,16 @@
 use super::*;
 use ark_ff::{biginteger::{BigInt, BigInteger64 as B1}, BigInteger};
-
 use ark_ff::PrimeField;
 use liblasso::utils::math::Math;
-
-use super::non_native_equalizer::bit_utils::{*, BitMath};
-
 use rayon::iter::plumbing::UnindexedConsumer;
 use rayon::prelude::*;
-
+use core::fmt;
 use std::ops::{Neg, AddAssign, Add, SubAssign, Sub, Mul,};
 use std::iter;
-
 use ark_std::{One, Zero};
+use std::fmt::Debug;
 
+use super::non_native_equalizer::bit_utils::{*, BitMath};
 use super::polynomial_with_zeros::{PolynomialWithZeros};
 
   
@@ -23,38 +20,51 @@ pub struct Splits<F: PrimeField> {
     pub rpolys: Vec<PolynomialWithZeros<F>>,
 }
 
-#[derive(Debug, Default, Clone)]
+#[derive(Clone)]
 pub struct NonNatOpen<FNat: PrimeField> {
-    polys: Vec<PolynomialWithZeros<FNat>>,
+    pub polys: Vec<PolynomialWithZeros<FNat>>,
     splits: Option<Splits<FNat>>,
     gamma: Option<FNat>,
     challenges: Vec<FNat>,
-    round_polys : Vec<UniPoly<FNat>> 
+    round_polys : Vec<UniPoly<FNat>>,
+    //f: PolynomialMapping<FNat>,
+    folded_f: Option<Arc<dyn Fn(&[FNat]) -> FNat + Sync + Send>>,
+    degree: usize,
+}
 
-    //copolys: Vec<Box<dyn Copolynomial<F> + Send + Sync>>,
-    //folded_f: Option<Arc<dyn Fn(&[F]) -> F + Sync + Send>>,
-    //degree: usize,
+impl<FNat: PrimeField> Debug for NonNatOpen<FNat>{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result{
+        f.debug_struct("Non-Native Open Protocol")
+         .field("input polynomials", &self.polys)
+         .field("challenges", &self.challenges)
+         .field("round_polynomials", &self.round_polys)
+         .finish()
+    }
 }
 
 impl<FNat : PrimeField> NonNatOpen<FNat>{
-    fn new_from_polys(polys : &[PolynomialWithZeros<FNat>]) -> Self{
+    pub fn new_from_polys(polys : &[PolynomialWithZeros<FNat>]) -> Self{
         NonNatOpen{
             polys : polys.into(),
             splits : None,
             gamma : None,
             challenges : vec![],
             round_polys : vec![],
+            folded_f : None,  
+            degree: 1,
         }
     }
 
-    fn new_from_evals(evals : &[&[FNat]]) -> Self{
+    pub fn new_from_evals(evals : &[&[FNat]]) -> Self{
         let polys = evals.iter().map(|&eval_vec| PolynomialWithZeros::new(eval_vec)).collect();
         NonNatOpen{
-            polys : polys.into(),
+            polys : polys,
             splits : None,
             gamma : None,
             challenges : vec![],
             round_polys : vec![],
+            folded_f : None,
+            degree: 1,
         }
     }
 }
@@ -88,51 +98,8 @@ impl<FNat: PrimeField> Sumcheckable<FNat> for NonNatOpen<FNat>{
         let lsum: Vec<_> = lpolys.iter().map(|p| p.sum()).collect();
         let rsum: Vec<_> = rpolys.iter().map(|p| p.sum()).collect();
 
-        // if lpolys.len() == 1{
-        //     let coeffs = ()
-        // }
-
-
-
-        // let poly_diffs = lpolys
-        //     .par_iter()
-        //     .zip(rpolys.par_iter())
-        //     .map(|(l, r)| {let mut r = r.clone(); r -= l; r})
-        //    .collect::<Vec<_>>();
-
         
-        // let degree = 1;
-        // let mut poly_extensions = Vec::with_capacity(degree);
 
-        // let mut last_poly = &rpolys;
-
-        // for i in 0..self.degree {
-        //     poly_extensions.push(last_poly.clone());
-        //     poly_extensions[i].par_iter_mut().zip(poly_diffs.par_iter()).map(|(p, d)| p.add_assign(d)).count();
-        //     last_poly = poly_extensions.last().unwrap();
-
-        //     copoly_extensions.push(last_copoly.clone());
-        //     copoly_extensions[i].par_iter_mut().zip(copoly_diffs.par_iter()).map(|(p, d)| p.add_assign(d)).count();
-        //     last_copoly = copoly_extensions.last().unwrap();
-        // }
-
-        // let folded = self.folded_f.as_ref().unwrap().clone();
-        // let poly_ext_iter = once(&lpolys).chain(once(&rpolys)).chain(poly_extensions.par_iter());
-        // let copoly_ext_iter = once(&lcopolys).chain(once(&rcopolys)).chain(copoly_extensions.par_iter());
-        // let results = poly_ext_iter.zip(copoly_ext_iter).map(|(polys, eqpolys)| {
-        //     let tmp = (0..polys[0].items_len()).into_par_iter().map(|i| {
-        //         folded(&polys.iter().map(|p| p[i]).chain(eqpolys.iter().map(|ep| ep[i])).collect_vec())
-        //     }).collect::<Vec<_>>();
-        //     tmp.par_iter().sum()
-        // }).collect::<Vec<F>>();
-
-        // self.splits = Some(Splits {
-        //     lpolys,
-        //     rpolys,
-        //     lcopolys,
-        //     rcopolys,
-        // });
-        // UniPoly::from_evals(&results);
         todo!();
 
     }
@@ -141,60 +108,3 @@ impl<FNat: PrimeField> Sumcheckable<FNat> for NonNatOpen<FNat>{
         self.polys.par_iter().map(|poly| poly.evals[0]).collect()
     }
 }
-
-
-mod tests{
-    use std::os::unix::thread;
-
-    use super::*;
-
-    use ark_bls12_381::Fr;
-    use ark_bls12_381::Fq;
-    use ark_bls12_381::G1Projective;
-    use ark_ff::{MontBackend};
-    use ark_std::{test_rng, UniformRand};
-    use rand::prelude::*;
-    use liblasso::poly;
-    use liblasso::utils::math::Math;
-    use crate::transcript::IndexedProofTranscript;
-    use liblasso::utils::test_lib::TestTranscript;
-
-    #[test]
-    fn test_nnat_open_split(){
-
-        let mut rng  = thread_rng();
-
-        let num_vars = 7;
-        let num_polys = 10;
-
-        let polys: Vec<PolynomialWithZeros<Fq>> = (0..num_polys)
-                .map(|_| PolynomialWithZeros::rand(&mut rng, thread_rng().gen_range(0..1<<num_vars), num_vars))
-                .collect();
-
-        let point: Vec<_> = (0..num_vars)
-                .map( |_| Fq::rand(&mut rng))
-                .collect();
-
-        let mut test_opener = NonNatOpen::new_from_polys(&polys);
-
-        let vals: Vec<_> = polys.clone().iter()
-                .map(|p| p.evaluate(&point))
-                .collect();
-
-        (0..num_vars).map(|i| test_opener.bind(&point[i])).count();
-
-        let final_evals = test_opener.final_evals();
-
-        (0..num_polys).map(|i| 
-            {
-                assert_eq!(test_opener.polys[i].evals.len(), 1);
-                assert_eq!(test_opener.polys[i].evals[0], vals[i]);
-                assert_eq!(final_evals[i], vals[i]);
-            })
-            .count();
-
-    }
-}
-
-
-
--- src/n_n_o/verifier.rs
+++ src/n_n_o/verifier.rs
@@ -1,134 +1,134 @@
-use super::*;
+// use super::*;
 
-impl<F: PrimeField> ProtocolVerifier<F> for NNSumcheckPolyMapVerifier<F> {
-    type Params = NNSumcheckPolyMapParams<F>;
+// impl<F: PrimeField> ProtocolVerifier<F> for NNSumcheckPolyMapVerifier<F> {
+//     type Params = NNSumcheckPolyMapParams<F>;
 
-    type ClaimsToReduce = MultiEvalClaim<F>;
+//     type ClaimsToReduce = MultiEvalClaim<F>;
 
-    type ClaimsNew = EvalClaim<F>;
+//     type ClaimsNew = EvalClaim<F>;
 
-    type Proof = NNSumcheckPolyMapProof<F>;
+//     type Proof = NNSumcheckPolyMapProof<F>;
 
-    fn start(
-        claims_to_reduce: Self::ClaimsToReduce,
-        proof: Self::Proof,
-        params: &Self::Params,
-    ) -> Self {
+//     fn start(
+//         claims_to_reduce: Self::ClaimsToReduce,
+//         proof: Self::Proof,
+//         params: &Self::Params,
+//     ) -> Self {
 
-        let f = params.f.clone();
+//         let f = params.f.clone();
 
-        let num_ins = f.num_i;
-        let num_outs = f.num_o;
-        let num_vars = params.num_vars;
-        let num_points = claims_to_reduce.points.len();
+//         let num_ins = f.num_i;
+//         let num_outs = f.num_o;
+//         let num_vars = params.num_vars;
+//         let num_points = claims_to_reduce.points.len();
 
-        // Validate that claims are well-formed.
+//         // Validate that claims are well-formed.
 
-        assert_eq!(
-            claims_to_reduce.evs.len(), num_points,
-            "Verifier failure. Claim ill-formed: number of points {} != number of evaluation groups {}", num_points, claims_to_reduce.evs.len()
-        );
+//         assert_eq!(
+//             claims_to_reduce.evs.len(), num_points,
+//             "Verifier failure. Claim ill-formed: number of points {} != number of evaluation groups {}", num_points, claims_to_reduce.evs.len()
+//         );
 
-        for (i, point) in claims_to_reduce.points.iter().enumerate() {
-            assert_eq!(
-                point.len(), num_vars,
-                "Verifier failure. Claim ill-formed: point {} has num variables {}, but declared num variables is {}", i, point.len(), num_vars
-            );
-        }
+//         for (i, point) in claims_to_reduce.points.iter().enumerate() {
+//             assert_eq!(
+//                 point.len(), num_vars,
+//                 "Verifier failure. Claim ill-formed: point {} has num variables {}, but declared num variables is {}", i, point.len(), num_vars
+//             );
+//         }
 
-        for ptevs in &claims_to_reduce.evs {
-            for ev in ptevs {
-                assert!(
-                    ev.0 < num_outs,
-                    "Verifier failure. Claim ill-formed: argument index {} out of range {}",
-                    ev.0,
-                    num_outs,
-                );
-            }
-        }
+//         for ptevs in &claims_to_reduce.evs {
+//             for ev in ptevs {
+//                 assert!(
+//                     ev.0 < num_outs,
+//                     "Verifier failure. Claim ill-formed: argument index {} out of range {}",
+//                     ev.0,
+//                     num_outs,
+//                 );
+//             }
+//         }
 
-        // Validate that proof is well-formed.
-        // We can not validate round_polys size here (compressed polynomial does not expose
-        // degree and I'm too lazy to vandalize liblasso once again),so this will be done during
-        // round function.
-        // TODO: Vandalize liblasso once again to expose this method ;)
+//         // Validate that proof is well-formed.
+//         // We can not validate round_polys size here (compressed polynomial does not expose
+//         // degree and I'm too lazy to vandalize liblasso once again),so this will be done during
+//         // round function.
+//         // TODO: Vandalize liblasso once again to expose this method ;)
 
-        assert_eq!(proof.round_polys.len(), num_vars);
-        assert_eq!(proof.final_evaluations.len(), num_ins);
+//         assert_eq!(proof.round_polys.len(), num_vars);
+//         assert_eq!(proof.final_evaluations.len(), num_ins);
 
-        Self {
-            proof,
-            f,
-            num_vars,
-            claims_to_reduce,
+//         Self {
+//             proof,
+//             f,
+//             num_vars,
+//             claims_to_reduce,
 
-            current_sum: None,
-            current_poly: None,
-            f_folded: None,
-            rs: vec![],
+//             current_sum: None,
+//             current_poly: None,
+//             f_folded: None,
+//             rs: vec![],
 
-        }
-    }
+//         }
+//     }
 
-    fn round<T: TranscriptReceiver<F>>(&mut self, challenge: Challenge<F>, transcript: &mut T)
-        ->
-    Option<Self::ClaimsNew> {
+//     fn round<T: TranscriptReceiver<F>>(&mut self, challenge: Challenge<F>, transcript: &mut T)
+//         ->
+//     Option<Self::ClaimsNew> {
 
-        let Self {current_sum, f, f_folded, num_vars, claims_to_reduce, proof, rs, current_poly} = self;
+//         let Self {current_sum, f, f_folded, num_vars, claims_to_reduce, proof, rs, current_poly} = self;
 
-        let NNSumcheckPolyMapProof { round_polys, final_evaluations } = proof;
+//         let NNSumcheckPolyMapProof { round_polys, final_evaluations } = proof;
 
-        assert!(rs.len() < *num_vars, "Verifier failure: attempt to call already finished verifier.");
+//         assert!(rs.len() < *num_vars, "Verifier failure: attempt to call already finished verifier.");
 
-        let sumcheck_round_idx;
-        // Detect 0-th round (gamma challenge).
-        if let None = current_sum {
-            let gamma = challenge.value;
-            let gamma_pows = make_gamma_pows(&claims_to_reduce, gamma);
-            *current_sum = Some(make_folded_claim(&claims_to_reduce, &gamma_pows));
-            *f_folded = Some(make_folded_f(&claims_to_reduce, &gamma_pows, &f));
-            sumcheck_round_idx = 0;
-        } else {
+//         let sumcheck_round_idx;
+//         // Detect 0-th round (gamma challenge).
+//         if let None = current_sum {
+//             let gamma = challenge.value;
+//             let gamma_pows = make_gamma_pows(&claims_to_reduce, gamma);
+//             *current_sum = Some(make_folded_claim(&claims_to_reduce, &gamma_pows));
+//             *f_folded = Some(make_folded_f(&claims_to_reduce, &gamma_pows, &f));
+//             sumcheck_round_idx = 0;
+//         } else {
 
-            let current_sum = current_sum.as_mut().unwrap();
-            let r_j = challenge.value;
-            fix_var_bot(rs, r_j);
+//             let current_sum = current_sum.as_mut().unwrap();
+//             let r_j = challenge.value;
+//             fix_var_bot(rs, r_j);
 
-            sumcheck_round_idx = rs.len();
+//             sumcheck_round_idx = rs.len();
 
-            // This unwrap never fails, because rounds after 0th always have the poly (which is last prover's message).
-            let current_poly = current_poly.as_ref().unwrap();
-            assert_eq!(
-                current_poly.degree(), f.degree + 1,
-                "Verifier failure: polynomial degree {} at round {} incorrect", current_poly.degree(), sumcheck_round_idx
-            );
+//             // This unwrap never fails, because rounds after 0th always have the poly (which is last prover's message).
+//             let current_poly = current_poly.as_ref().unwrap();
+//             assert_eq!(
+//                 current_poly.degree(), f.degree + 1,
+//                 "Verifier failure: polynomial degree {} at round {} incorrect", current_poly.degree(), sumcheck_round_idx
+//             );
 
-            *current_sum = current_poly.evaluate(&r_j);
+//             *current_sum = current_poly.evaluate(&r_j);
 
-            if rs.len() == *num_vars {
+//             if rs.len() == *num_vars {
 
-                transcript.append_scalars(b"sumcheck_final_evals", &final_evaluations[0..f.num_i]);
+//                 transcript.append_scalars(b"sumcheck_final_evals", &final_evaluations[0..f.num_i]);
 
-                // Cloning final evals to not change the proof. We probably do not need it, as we consume it anyway.
-                let mut args = final_evaluations.clone();
-                args.extend(claims_to_reduce.points.iter().map(|p| EqPolynomial::new(p.to_vec()).evaluate(&rs)));
+//                 // Cloning final evals to not change the proof. We probably do not need it, as we consume it anyway.
+//                 let mut args = final_evaluations.clone();
+//                 args.extend(claims_to_reduce.points.iter().map(|p| EqPolynomial::new(p.to_vec()).evaluate(&rs)));
 
-                assert_eq!((f_folded.as_ref().unwrap())(&args), *current_sum, "Verifier failure: final check incorrect");
+//                 assert_eq!((f_folded.as_ref().unwrap())(&args), *current_sum, "Verifier failure: final check incorrect");
 
-                return Some(
-                    EvalClaim{
-                        point: rs.clone(),
-                        evs: final_evaluations.clone(),
-                    }
-                )
-            }
-        }
+//                 return Some(
+//                     EvalClaim{
+//                         point: rs.clone(),
+//                         evs: final_evaluations.clone(),
+//                     }
+//                 )
+//             }
+//         }
 
-        let new_poly = round_polys[sumcheck_round_idx].decompress(&current_sum.unwrap());
-        // This indexing never fails, because n-th round will return from the else clause.
-        transcript.append_scalars(b"poly", &new_poly.as_vec());
-        *current_poly = Some(new_poly);
+//         let new_poly = round_polys[sumcheck_round_idx].decompress(&current_sum.unwrap());
+//         // This indexing never fails, because n-th round will return from the else clause.
+//         transcript.append_scalars(b"poly", &new_poly.as_vec());
+//         *current_poly = Some(new_poly);
 
-        None
-    }
-}
+//         None
+//     }
+// }

1043e8a9e0794fc924cdd3d0b04980042e95ba3a
yuliakot <yuliakotel@gmail.com>
2024-10-10 13:29:42 -0400

    improved addition of polynomials

--- src/n_n_o/tests.rs
+++ src/n_n_o/tests.rs
@@ -5,78 +5,94 @@ use super::non_native_equalizer::*;
 use ark_bls12_381::Fr;
 use ark_bls12_381::Fq;
 use ark_bls12_381::G1Projective;
+use ark_ec::scalar_mul;
 use ark_ff::{MontBackend, PrimeField};
 use ark_std::{test_rng, UniformRand};
 use ark_std::rand::Rng;
 use liblasso::poly;
 use liblasso::utils::math::Math;
+
+use rayon::iter::plumbing::UnindexedConsumer;
+use rayon::prelude::*;
+
+use std::iter;
 use crate::transcript::IndexedProofTranscript;
 use liblasso::utils::test_lib::TestTranscript;
+use ark_std::Zero;
+
+// #[test]
+// fn test_polynomial_new(){
+
+//     let mut rng  = test_rng();
+//     let num_limbs = 3;
+//     let limb_len = roundup_to_pow2( Fq::MODULUS_BIT_SIZE  as usize / num_limbs);
+
+//     let poly_size = 6u64;
+//     let log_poly_size = 3u64;
+
+//     let poly1: Vec<_> =  (0..poly_size).map(|i| Fq::from(i)).collect();
+//     let poly2: Vec<_> =  (0..poly_size).map(|i| Fq::from(i+1)).collect();
+//     // let r_bits = big_num_to_bits_F(r);
+//     let point: Vec<_> = (0..log_poly_size).map(|i| Fq::from(2*i + 1)).collect();
+
+//     let p1 = PolynomialWithZeros::new(&poly1);
+//     let value1 = p1.evaluate(&point);
+    
+//     let p2 = PolynomialWithZeros::new(&poly2);
+//     let value2 = p2.evaluate(&point);
+
+//     assert_eq!(p1.len, poly_size as usize);
+//     assert_eq!(p1.log_len, log_poly_size as usize);
+//     assert_eq!(value1 +  Fq::from(78), Fq::from(0));
+//     assert_eq!(value2 +  Fq::from(78)+  Fq::from(15), Fq::from(1));
+
+// }
+
 
 #[test]
-fn test_polynomial_new(){
+fn test_polynomial_sum_1d(){
 
     let mut rng  = test_rng();
     let num_limbs = 3;
     let limb_len = roundup_to_pow2( Fq::MODULUS_BIT_SIZE  as usize / num_limbs);
 
-    let poly_size = 6u64;
-    let log_poly_size = 3u64;
+    let poly_size1 = rand::thread_rng().gen_range(0..128);
+    let poly_size2 = rand::thread_rng().gen_range(0..128);
+    let log_poly_size = 7;
 
-    let poly1: Vec<_> =  (0..poly_size).map(|i| Fq::from(i)).collect();
-    let poly2: Vec<_> =  (0..poly_size).map(|i| Fq::from(i+1)).collect();
-    // let r_bits = big_num_to_bits_F(r);
-    let point: Vec<_> = (0..log_poly_size).map(|i| Fq::from(2*i + 1)).collect();
+    for _ in 0..10{
+        let p1 = PolynomialWithZeros::rand(&mut rng, poly_size1, log_poly_size);
+        let p2 = PolynomialWithZeros::rand(&mut rng, poly_size2, log_poly_size);
 
-    let p1 = PolynomialWithZeros::new(&poly1);
-    let value1 = p1.evaluate(&point);
+        let p1_evals = p1.clone().evals;
+        let p2_evals = p2.clone().evals;
+
+        let mut sum_p = p1.clone();
+        sum_p += &p2;
+        let sum_evals = sum_p.evals;
+        let mut diff_p = p2.clone();
+        diff_p -= &p1;
+        let diff_evals = diff_p.evals;
+
+        let check_sum = p1_evals.iter()
+                                        .zip(p2_evals.iter()
+                                                    .chain(iter::repeat(&Fq::zero())))
+                                        .zip(sum_evals.iter()
+                                                    .chain(iter::repeat(&Fq::zero())))
+                                        .all(|((a, b), c)| a.clone() + b.clone() - c.clone() == Fq::zero());
+
+        assert!(check_sum, "sum bad");
+
+        
+        let check_diff = p1_evals.iter()
+                                        .zip(p2_evals.iter()
+                                                    .chain(iter::repeat(&Fq::zero())))
+                                        .zip(diff_evals.iter()
+                                                    .chain(iter::repeat(&Fq::zero())))
+                                        .all(|((a, b), c)| b.clone() - a.clone() - c.clone() == Fq::zero());
     
-    let p2 = PolynomialWithZeros::new(&poly2);
-    let value2 = p2.evaluate(&point);
-
-    assert_eq!(p1.len, poly_size as usize);
-    assert_eq!(p1.log_len, log_poly_size as usize);
-    assert_eq!(value1 +  Fq::from(78), Fq::from(0));
-    assert_eq!(value2 +  Fq::from(78)+  Fq::from(15), Fq::from(1));
-
-}
-
-
-#[test]
-fn test_polynomial_split_and_bind(){
-
-    let mut rng  = test_rng();
-    let num_limbs = 3;
-    let limb_len = roundup_to_pow2( Fq::MODULUS_BIT_SIZE  as usize / num_limbs);
-
-    let poly_size = 6u64;
-    let log_poly_size = 3u64;
-
-    let poly1: Vec<_> =  (0..poly_size).map(|i| Fq::from(i)).collect();
-    let poly2: Vec<_> =  (0..poly_size + 1).map(|i| Fq::from(i+1)).collect();
-    
-    let p1 = PolynomialWithZeros::new(&poly1);
-    let p2 = PolynomialWithZeros::new(&poly2);
-
-    let mut sum_p = p1.clone();
-    sum_p += &p2;
-    let mut diff_p = p2.clone();
-    diff_p -= &p1;
-
-    let bind_1 = p1.bind(&Fq::from(2));
-    let bind_2 = p2.bind(&Fq::from(2));
-
-    let sum_bind = sum_p.bind(&Fq::from(2));
-
-
-
-    println!("sum_p = {:?}, diff_p = {:?}\n", sum_p, diff_p);
-    
-    println!("bind1 = {:?},  bind_2 = {:?}\n", bind_1.clone(), bind_2.clone());
-    println!("bind1 + bind2 = {:?},  sum_bind = {:?}", bind_1.clone() + bind_2.clone(), sum_bind);
-
-    
-    assert_eq!( bind_1.clone() + bind_2.clone(), sum_bind);
+        assert!(check_diff, "diff bad");
+    }
 }
 
 
